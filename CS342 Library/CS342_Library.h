7f04263ca3198320bed975a7f4886220824ad06a
Santiago Gutierrez <ssgutierrez42@gmail.com>
2019-09-01 15:29:09 -0700

    Initial Commit

---
+++ CS342 Library/CS342_Library.h
@@ -0,0 +1,19 @@
+//
+//  CS342_Library.h
+//  CS342 Library
+//
+//  Created by Santiago Gutierrez on 9/1/19.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CS342_Library.
+FOUNDATION_EXPORT double CS342_LibraryVersionNumber;
+
+//! Project version string for CS342_Library.
+FOUNDATION_EXPORT const unsigned char CS342_LibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CS342_Library/PublicHeader.h>
+
+
