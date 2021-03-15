1aa06d3563250185e7a902f30f0e469e3dcf2fbc
Collin Schlager <schlagercollin@gmail.com>
2021-03-14 23:43:14 -0700

    Add dynamic color for drug plots and minor UI fixes
    * Changes <drugs> taken to <drug> to save space in figure legend
    * Created plot function that maps data series index to a color

--- CardinalKit-Example/CardinalKit/Components/Schedule/ScheduleViewController.swift
+++ CardinalKit-Example/CardinalKit/Components/Schedule/ScheduleViewController.swift
@@ -84,6 +84,23 @@ class ScheduleViewController: OCKDailyPageViewController {
         HKHealthStore().execute(diastolicQuery)
     }
     
+    // Helper function to return some plot colors for different series
+    // Note: if done in the loop, the color gets overwritten between
+    // loops; creating the color object here seems to prevent this issues
+    func getDrugColor(i: Int) -> UIColor {
+        
+        // Hard code some colors for variety if |drugs| > 1
+        let drugColorsLight = [#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)]
+        let drugColorsDark =  [#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1), #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)]
+        let count = min(drugColorsLight.count, drugColorsDark.count)
+        
+        let idx = i % count
+        
+        return UIColor { traitCollection -> UIColor in
+            return traitCollection.userInterfaceStyle == .light ? drugColorsLight[idx] : drugColorsLight[idx]
+        }
+    }
+    
     func generateLastWeekPressureGraph(date: Date) -> OCKCartesianChartView {
         let bloodPressureSystolicGradientStart = UIColor { traitCollection -> UIColor in
             return traitCollection.userInterfaceStyle == .light ? #colorLiteral(red: 0.9960784314, green: 0.3725490196, blue: 0.368627451, alpha: 1) : #colorLiteral(red: 0.8627432641, green: 0.2630574384, blue: 0.2592858295, alpha: 1)
@@ -247,17 +264,21 @@ class ScheduleViewController: OCKDailyPageViewController {
                     var totalDataSeries = [diaryDataSeries]
                     
                     if let medicationDictionary = SupplementalUserInformation.shared.retrieveSupplementalDictionary()?["medications"] as? Dictionary<String, Int> {
-                        
+                                                
+                        var i = 0
                         for medicationName in medicationDictionary.keys {
+                            print("Plot\(i)")
+                            print("Color\(getDrugColor(i: i))")
                             let drugDataSeries = OCKDataSeriesConfiguration(
                                 taskID: "drug\(medicationName)",
-                                legendTitle: "\(medicationName) Taken",
-                                gradientStartColor: .systemGray2,
-                                gradientEndColor: .systemGray,
+                                legendTitle: "\(medicationName)",
+                                gradientStartColor: getDrugColor(i: i),
+                                gradientEndColor: getDrugColor(i: i),
                                 markerSize: 10,
                                 eventAggregator: OCKEventAggregator.countOutcomeValues)
                             
                             totalDataSeries.append(drugDataSeries)
+                            i = i + 1;
                         }
                     }
                     
