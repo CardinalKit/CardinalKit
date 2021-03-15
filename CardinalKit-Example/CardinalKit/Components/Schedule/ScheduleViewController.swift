7942e1f8251f33c89c0fc9877aea3d9b920f1a47
Kabir Jolly <kabir.s.jolly@gmail.com>
2021-03-14 23:13:06 -0500

    Added BP cuff integration from the Cardiology team codebase

--- CardinalKit-Example/CardinalKit/Components/Schedule/ScheduleViewController.swift
+++ CardinalKit-Example/CardinalKit/Components/Schedule/ScheduleViewController.swift
@@ -8,8 +8,11 @@
 
 import CareKit
 import CareKitStore
+import CareKitUI
 import UIKit
 import SwiftUI
+import HealthKit
+import HealthKitUI
 
 class ScheduleViewController: OCKDailyPageViewController {
     
@@ -18,14 +21,122 @@ class ScheduleViewController: OCKDailyPageViewController {
         title = "Schedule"
     }
     
+    var systolicDoubleArray: [CGFloat] = []
+    var diastolicDoubleArray: [CGFloat] = []
+    
+    func queryHealthkitLastWeekBPDataStatistic(date: Date) {
+        let now = date
+        let startDate = Calendar.current.date(byAdding: .day, value: -6, to: now)!
+        
+        var interval = DateComponents()
+        interval.day = 1
+        
+        var anchorComponents = Calendar.current.dateComponents([.day, .month, .year], from: now)
+        anchorComponents.hour = 0
+        let anchorDate = Calendar.current.date(from: anchorComponents)!
+        
+        let systolicQuery = HKStatisticsCollectionQuery(quantityType: HKObjectType.quantityType(forIdentifier: .bloodPressureSystolic)!,
+                                                        quantitySamplePredicate: nil,
+                                                        options: .discreteAverage,
+                                                        anchorDate: anchorDate,
+                                                        intervalComponents: interval)
+        
+        systolicQuery.initialResultsHandler = {_, results, error in
+            print("QUERY RETURNED")
+            guard let results = results else {
+                print("Error returned form resultHandler = \(String(describing: error?.localizedDescription))")
+                return
+            }
+            self.systolicDoubleArray = []
+            results.enumerateStatistics(from: startDate, to: now) { statistics, _ in
+                if let average = statistics.averageQuantity() {
+                    let avg = average.doubleValue(for: HKUnit.millimeterOfMercury())
+                    self.systolicDoubleArray.append(CGFloat(avg))
+                } else {
+                    self.systolicDoubleArray.append(0)
+                }
+            }
+        }
+        
+        let diastolicQuery = HKStatisticsCollectionQuery(quantityType: HKObjectType.quantityType(forIdentifier: .bloodPressureDiastolic)!,
+                                                        quantitySamplePredicate: nil,
+                                                        options: .discreteAverage,
+                                                        anchorDate: anchorDate,
+                                                        intervalComponents: interval)
+        
+        diastolicQuery.initialResultsHandler = {_, results, error in
+            guard let results = results else {
+                print("Error returned form resultHandler = \(String(describing: error?.localizedDescription))")
+                return
+            }
+            self.diastolicDoubleArray = []
+            results.enumerateStatistics(from: startDate, to: now) { statistics, _ in
+                if let average = statistics.averageQuantity() {
+                    let avg = average.doubleValue(for: HKUnit.millimeterOfMercury())
+                    self.diastolicDoubleArray.append(CGFloat(avg))
+                } else {
+                    self.diastolicDoubleArray.append(0)
+                }
+            }
+        }
+        
+        HKHealthStore().execute(systolicQuery)
+        HKHealthStore().execute(diastolicQuery)
+    }
+    
+    func generateLastWeekPressureGraph(date: Date) -> OCKCartesianChartView {
+        let bloodPressureSystolicGradientStart = UIColor { traitCollection -> UIColor in
+            return traitCollection.userInterfaceStyle == .light ? #colorLiteral(red: 0.9960784314, green: 0.3725490196, blue: 0.368627451, alpha: 1) : #colorLiteral(red: 0.8627432641, green: 0.2630574384, blue: 0.2592858295, alpha: 1)
+        }
+        let bloodPressureSystolicGradientEnd = UIColor { traitCollection -> UIColor in
+            return traitCollection.userInterfaceStyle == .light ? #colorLiteral(red: 0.9960784314, green: 0.4732026144, blue: 0.368627451, alpha: 1) : #colorLiteral(red: 0.8627432641, green: 0.3598620686, blue: 0.2592858295, alpha: 1)
+        }
+        
+        let bloodPressureDiastolicGradientStart = UIColor { traitCollection -> UIColor in
+            return traitCollection.userInterfaceStyle == .light ? #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1) : #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        }
+        let bloodPressureDiastolicGradientEnd = UIColor { traitCollection -> UIColor in
+            return traitCollection.userInterfaceStyle == .light ? #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        }
+        
+        let chartView = OCKCartesianChartView(type: .bar)
+        
+        chartView.headerView.titleLabel.text = "Your blood pressure over the last week"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EE"
+        let lastWeekDateArray = [date.advanced(by: -24 * 60 * 60 * 6),
+                                 date.advanced(by: -24 * 60 * 60 * 5),
+                                 date.advanced(by: -24 * 60 * 60 * 4),
+                                 date.advanced(by: -24 * 60 * 60 * 3),
+                                 date.advanced(by: -24 * 60 * 60 * 2),
+                                 date.advanced(by: -24 * 60 * 60 * 1),
+                                 date]
+        let lastWeekDateArrayLabels = lastWeekDateArray.map { (date) -> String in
+            return dateFormatter.string(from: date)
+        }
+        
+        let systolicDataSeries = OCKDataSeries(values: systolicDoubleArray, title: "Systolic", gradientStartColor: bloodPressureSystolicGradientStart, gradientEndColor: bloodPressureSystolicGradientEnd)
+        
+        let diastolicDataSeries = OCKDataSeries(values: diastolicDoubleArray, title: "Diastolic", gradientStartColor: bloodPressureDiastolicGradientStart, gradientEndColor: bloodPressureDiastolicGradientEnd)
+        
+        chartView.graphView.dataSeries = [systolicDataSeries, diastolicDataSeries]
+        chartView.graphView.horizontalAxisMarkers = lastWeekDateArrayLabels
+        chartView.graphView.yMinimum = 0
+        chartView.graphView.yMaximum = 220
+        
+        return chartView
+    }
+    
     override func dailyPageViewController(_ dailyPageViewController: OCKDailyPageViewController, prepare listViewController: OCKListViewController, for date: Date) {
         
-        let identifiers = ["doxylamine", "medication", "nausea", "coffee", "survey", "steps", "heartRate", "sf12", "check-in", "tremor", "prograf", "tremor-log"]
+        let identifiers = ["doxylamine", "medication", "nausea", "coffee", "survey", "steps", "heartRate", "sf12", "check-in", "tremor", "prograf", "tremor-log", "heartrate-2", "bloodpressure"]
         var query = OCKTaskQuery(for: date)
         query.ids = identifiers
         query.excludesTasksWithNoEvents = true
+        queryHealthkitLastWeekBPDataStatistic(date: date)
 
-        storeManager.store.fetchAnyTasks(query: query, callbackQueue: .main) { result in
+        storeManager.store.fetchAnyTasks(query: query, callbackQueue: .main) { [self] result in
             switch result {
             case .failure(let error): print("Error: \(error)")
             case .success(let tasks):
@@ -41,6 +152,13 @@ class ScheduleViewController: OCKDailyPageViewController {
 //                    tipView.imageView.image = UIImage(named: "GraphicOperatingSystem")
 //                    listViewController.appendView(tipView, animated: false)
 //                }
+                if (diastolicDoubleArray.isEmpty || systolicDoubleArray.isEmpty) {
+                    print("Error: No blood pressure data found")
+                } else if date < Date() {
+                    // we don't want to graph in the future, as the HKQueries won't work properly
+                    let lastWeekDataCard = generateLastWeekPressureGraph(date: date)
+                    listViewController.appendView(lastWeekDataCard, animated: false)
+                }
 
                 if #available(iOS 14, *), let walkTask = tasks.first(where: { $0.id == "steps" }) {
 
@@ -142,84 +260,28 @@ class ScheduleViewController: OCKDailyPageViewController {
                     listViewController.appendViewController(tremorLogCard, animated: false)
                     listViewController.appendViewController(insightsCard, animated: false)
                 }
-
-                // Since the coffee task is only scheduled every other day, there will be cases
-                // where it is not contained in the tasks array returned from the query.
-//                if let coffeeTask = tasks.first(where: { $0.id == "coffee" }) {
-//                    let coffeeCard = OCKSimpleTaskViewController(task: coffeeTask, eventQuery: .init(for: date),
-//                                                                 storeManager: self.storeManager)
-//                    listViewController.appendViewController(coffeeCard, animated: false)
-//                }
-//
-//                if let surveyTask = tasks.first(where: { $0.id == "survey" }) {
-//                    let surveyCard = SurveyItemViewController(
-//                        viewSynchronizer: SurveyItemViewSynchronizer(),
-//                        task: surveyTask,
-//                        eventQuery: .init(for: date),
-//                        storeManager: self.storeManager)
-//
-//                    listViewController.appendViewController(surveyCard, animated: false)
-//                }
-//
-//                // Create a card for the water task if there are events for it on this day.
-//                if let doxylamineTask = tasks.first(where: { $0.id == "doxylamine" }) {
-//
-//                    let doxylamineCard = OCKChecklistTaskViewController(
-//                        task: doxylamineTask,
-//                        eventQuery: .init(for: date),
-//                        storeManager: self.storeManager)
-//
-//                    listViewController.appendViewController(doxylamineCard, animated: false)
-//                }
-//
-//                // Create a card for the nausea task if there are events for it on this day.
-//                // Its OCKSchedule was defined to have daily events, so this task should be
-//                // found in `tasks` every day after the task start date.
-//                if let nauseaTask = tasks.first(where: { $0.id == "nausea" }) {
-//
-//                    // dynamic gradient colors
-//                    let nauseaGradientStart = UIColor { traitCollection -> UIColor in
-//                        return traitCollection.userInterfaceStyle == .light ? #colorLiteral(red: 0.9960784314, green: 0.3725490196, blue: 0.368627451, alpha: 1) : #colorLiteral(red: 0.8627432641, green: 0.2630574384, blue: 0.2592858295, alpha: 1)
-//                    }
-//                    let nauseaGradientEnd = UIColor { traitCollection -> UIColor in
-//                        return traitCollection.userInterfaceStyle == .light ? #colorLiteral(red: 0.9960784314, green: 0.4732026144, blue: 0.368627451, alpha: 1) : #colorLiteral(red: 0.8627432641, green: 0.3598620686, blue: 0.2592858295, alpha: 1)
-//                    }
-//
-//                    // Create a plot comparing nausea to medication adherence.
-//                    let nauseaDataSeries = OCKDataSeriesConfiguration(
-//                        taskID: "nausea",
-//                        legendTitle: "Nausea",
-//                        gradientStartColor: nauseaGradientStart,
-//                        gradientEndColor: nauseaGradientEnd,
-//                        markerSize: 10,
-//                        eventAggregator: OCKEventAggregator.countOutcomeValues)
-//
-//                    let doxylamineDataSeries = OCKDataSeriesConfiguration(
-//                        taskID: "doxylamine",
-//                        legendTitle: "Doxylamine",
-//                        gradientStartColor: .systemGray2,
-//                        gradientEndColor: .systemGray,
-//                        markerSize: 10,
-//                        eventAggregator: OCKEventAggregator.countOutcomeValues)
-//
-//                    let insightsCard = OCKCartesianChartViewController(
-//                        plotType: .bar,
-//                        selectedDate: date,
-//                        configurations: [nauseaDataSeries, doxylamineDataSeries],
-//                        storeManager: self.storeManager)
-//
-//                    insightsCard.chartView.headerView.titleLabel.text = "Nausea & Doxylamine Intake"
-//                    insightsCard.chartView.headerView.detailLabel.text = "This Week"
-//                    insightsCard.chartView.headerView.accessibilityLabel = "Nausea & Doxylamine Intake, This Week"
-//                    listViewController.appendViewController(insightsCard, animated: false)
-//
-//                    // Also create a card that displays a single event.
-//                    // The event query passed into the initializer specifies that only
-//                    // today's log entries should be displayed by this log task view controller.
-//                    let nauseaCard = OCKButtonLogTaskViewController(task: nauseaTask, eventQuery: .init(for: date),
-//                                                                    storeManager: self.storeManager)
-//                    listViewController.appendViewController(nauseaCard, animated: false)
-//                }
+                
+                if let heartrateTask = tasks.first(where: { $0.id == "heartrate-2" }) {
+                    let surveyCard = BloodPressureItemViewController(
+                        viewSynchronizer: BloodPressureItemViewSynchronizer(),
+                        task: heartrateTask,
+                        eventQuery: .init(for: date),
+                        storeManager: self.storeManager)
+                    
+                    listViewController.appendViewController(surveyCard, animated: false)
+                }
+                
+                if let bloodpressureTask = tasks.first(where: { $0.id == "bloodpressure" }) {
+                    let surveyCard = BloodPressureItemViewController(
+                        viewSynchronizer: BloodPressureItemViewSynchronizer(),
+                        task: bloodpressureTask,
+                        eventQuery: .init(for: date),
+                        storeManager: self.storeManager)
+                    
+                    listViewController.appendViewController(surveyCard, animated: false)
+                } else {
+                    print("Couldn't find the task!")
+                }
             }
         }
     }
