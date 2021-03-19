27bc1eddd40e70a9569b8c8a7633c615e25efb56
cooper-mj <coopermj@stanford.edu>
2021-03-18 23:34:07 -0700

    Prototypical beacon ranging - testing needed

---
+++ CardinalKit-Example/CardinalKit/Components/SetupPushNotifications.swift
@@ -0,0 +1,9 @@
+//
+//  SetupPushNotifications.swift
+//  CardinalKit_Example
+//
+//  Created by Michael Cooper on 2021-03-17.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
