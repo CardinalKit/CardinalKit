73bd3533a91a49887f30c338d049d6acdcb81179
Vishnu Ravi <vishnu.ravi@gmail.com>
2021-10-24 21:53:35 -0400

    Fixes CareKit layout errors (#75)

--- CardinalKit-Example/CardinalKit/Components/MainUIView.swift
+++ CardinalKit-Example/CardinalKit/Components/MainUIView.swift
@@ -27,14 +27,18 @@ struct MainUIView: View {
             }
             
             if useCareKit {
-                ScheduleViewControllerRepresentable().tabItem {
-                    Image("tab_schedule").renderingMode(.template)
-                    Text("Schedule")
+                ScheduleViewControllerRepresentable()
+                    .ignoresSafeArea(edges: .all)
+                    .tabItem {
+                        Image("tab_schedule").renderingMode(.template)
+                        Text("Schedule")
                 }
                 
-                CareTeamViewControllerRepresentable().tabItem {
-                    Image("tab_care").renderingMode(.template)
-                    Text("Contact")
+                CareTeamViewControllerRepresentable()
+                    .ignoresSafeArea(edges: .all)
+                    .tabItem {
+                        Image("tab_care").renderingMode(.template)
+                        Text("Contact")
                 }
             }
 
