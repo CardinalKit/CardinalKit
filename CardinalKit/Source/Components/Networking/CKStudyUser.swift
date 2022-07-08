0ec1ed19756bdb1ee7b7a5d7e58333575ef4db6f
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-07-08 08:24:08 -0400

    Removes redundant CKStudyUser

--- CardinalKit/Source/Components/Networking/CKStudyUser.swift
+++ CardinalKit/Source/Components/Networking/CKStudyUser.swift
@@ -2,59 +2,59 @@
 //  CKStudyUser.swift
 //  CardinalKit
 //
-//  Created by Julian Esteban Ramos Martinez on 7/01/22.
+//  Created by Vishnu Ravi on 7/8/22.
 //
 
 import Foundation
 import FirebaseAuth
 import FirebaseFirestore
 
-class CKStudyUser {
-    
+class CKStudyUser: ObservableObject {
+
     static let shared = CKStudyUser()
-    
+
+    private weak var authStateHandle: AuthStateDidChangeListenerHandle?
+
     /* **************************************************************
      * the current user only resolves if we are logged in
-    **************************************************************/
+     **************************************************************/
     var currentUser: User? {
-        // this is a reference to the
-        // Firebase + Google Identity User
         return Auth.auth().currentUser
     }
-    
+
     /* **************************************************************
      * store your Firebase objects under this path in order to
      * be compatible with CardinalKit GCP rules.
-    **************************************************************/
+     **************************************************************/
     var authCollection: String? {
         if let userId = currentUser?.uid,
-            let root = rootAuthCollection {
+           let root = rootAuthCollection {
             return "\(root)\(userId)/"
         }
-        
+
         return nil
     }
-    
+
     var surveysCollection: String? {
         if let bundleId = Bundle.main.bundleIdentifier {
             return "/studies/\(bundleId)/surveys/"
         }
-        
+
         return nil
     }
-    
+
     var studyCollection: String?{
         if let bundleId = Bundle.main.bundleIdentifier {
             return "/studies/\(bundleId)/"
         }
         return nil
     }
-    
+
     fileprivate var rootAuthCollection: String? {
         if let bundleId = Bundle.main.bundleIdentifier {
             return "/studies/\(bundleId)/users/"
         }
-        
+
         return nil
     }
 
@@ -70,89 +70,81 @@ class CKStudyUser {
             }
         }
     }
-    
+
     var isLoggedIn: Bool {
         return (currentUser?.isEmailVerified ?? false) && UserDefaults.standard.bool(forKey: Constants.UserDefaults.prefConfirmedLogin)
     }
-    
+
+    init() {
+        // listen for changes in authentication state from Firebase and update currentUser
+        authStateHandle = Auth.auth().addStateDidChangeListener { [weak self] (_, _) in
+            self?.objectWillChange.send()
+        }
+    }
+
+    deinit {
+        // remove the authentication state handle when the instance is deallocated
+        if let handle = authStateHandle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
+
     /**
-    Send a login email to the user.
+     Send a login email to the user.
 
-    At this stage, we do not have a `currentUser` via Google Identity.
+     At this stage, we do not have a `currentUser` via Google Identity.
 
-    - Parameters:
-        - email: validated address that should receive the sign-in link.
-        - completion: callback
-    */
+     - Parameters:
+     - email: validated address that should receive the sign-in link.
+     - completion: callback
+     */
     func sendLoginLink(email: String, completion: @escaping (Bool)->Void) {
         guard !email.isEmpty else {
             completion(false)
             return
         }
-        
+
         let actionCodeSettings = ActionCodeSettings()
         actionCodeSettings.url = URL(string: "https://cs342.page.link")
         actionCodeSettings.handleCodeInApp = true // The sign-in operation has to always be completed in the app.
         actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
-        
+
         Auth.auth().sendSignInLink(toEmail: email, actionCodeSettings: actionCodeSettings) { (error) in
             if let error = error {
                 print(error.localizedDescription)
                 completion(false)
                 return
             }
-            
+
             completion(true)
         }
     }
 
     /**
-    Save a snapshot of our current user into Firestore.
-    */
+     Save a snapshot of our current user into Firestore.
+     */
     func save() {
         if let dataBucket = rootAuthCollection,
-            let email = currentUser?.email,
-            let uid = currentUser?.uid {
-            
+           let email = currentUser?.email,
+           let uid = currentUser?.uid {
+
             CKSession.shared.userId = uid
-            createNecessaryDocuments(path:dataBucket)
+            CKSendHelper.createNecessaryDocuments(path:dataBucket)
             let settings = FirestoreSettings()
             settings.isPersistenceEnabled = false
             let db = Firestore.firestore()
             db.settings = settings
-            db.collection(dataBucket).document(uid).setData(["userID":uid, "lastActive":Date().ISOStringFromDate(),"email":email])
+            db.collection(dataBucket).document(uid).setData(["userID":uid, "lastActive":Date().ISOStringFromDate(),"email":email], merge: true)
         }
     }
-    
+
     /**
-    Remove the current user's auth parameters from storage.
-    */
+     Remove the current user's auth parameters from storage.
+     */
     func signOut() throws {
         email = nil
         try Auth.auth().signOut()
     }
-    
-    func createNecessaryDocuments(path: String){
-        let _db=firestoreDb()
-        let _pathArray = path.split{$0 == "/"}.map(String.init)
-        var currentPath = ""
-        var index=0
-        for part in _pathArray{
-            currentPath+=part
-            if(index%2 != 0){
-                _db.document(currentPath).setData(["exist":"true"], merge: true)
-            }
-            currentPath+="/"
-            index+=1
-        }
-    }
-    
-    func firestoreDb()->Firestore{
-        let settings = FirestoreSettings()
-        settings.isPersistenceEnabled = false
-        let db = Firestore.firestore()
-        db.settings = settings
-        return db
-    }
-    
+
 }
+
